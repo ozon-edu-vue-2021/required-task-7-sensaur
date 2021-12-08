7738996760a41f53f4de77dd79508bb49fb3e786
Ilya Zhigarev <zhigarev@bk.ru>
2021-12-09 00:00:51 +0300

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,97 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app light>
+    <v-toolbar app color="blue-grey darken-3" class="white--text">
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">My</span>
+        <span>Friends</span>
+        <span class="font-weight-light">List</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-tooltip left>
+        <v-btn @click="meetSomeone('male')" slot="activator" icon class="white--text">
+          <v-icon>fa-mars</v-icon>
+        </v-btn>
+        <span>Introduce me to a new Dude!</span>
+      </v-tooltip>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      <v-tooltip left>
+        <v-btn @click="meetSomeone('female')" slot="activator" icon class="white--text">
+          <v-icon>fa-venus</v-icon>
+        </v-btn>
+        <span>Introduce me to a new Lady!!</span>
+      </v-tooltip>
+    </v-toolbar>
+    <v-content>
+      <FriendList v-bind:friends="friends"/>
+    </v-content>
+    <v-snackbar
+        v-model="snackbar"
+        :bottom="y === 'bottom'"
+        :left="x === 'left'"
+        :multi-line="mode === 'multi-line'"
+        :right="x === 'right'"
+        :timeout="timeout"
+        :top="y === 'top'"
+        :vertical="mode === 'vertical'"
+        :color="toast"
+    >{{ text }}</v-snackbar>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import FriendList from "./components/FriendList";
+import axios from "axios";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    FriendList
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      friends: [],
+      snackbar: false,
+      y: "bottom",
+      x: "left",
+      mode: "",
+      timeout: 3000,
+      text: "",
+      toast: ""
+    };
+  },
+
+  mounted() {
+    this.friends = JSON.parse(window.localStorage.getItem("friends")) || [];
+  },
+
+  methods: {
+    async meetSomeone(gender) {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: `http://localhost:1331/friend/${gender}`,
+          headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2UiOiJkZW1vIiwiaWF0IjoxNTQ0MTMzNjc2fQ.bMZTSEdftqNjzlkRBDF9fuFJWPd5tv3x84ATVPxjVuE"
+          }
+        });
+        this.friends.push(data.friend);
+        window.localStorage.setItem("friends", JSON.stringify(this.friends));
+      } catch (error) {
+        this.toast = "red";
+        this.text = `An error has ocurred: ${error.message}`;
+        this.snackbar = true;
+      }
+    }
+  },
+
+  watch: {
+    friends() {
+      this.toast = "green";
+      this.text = "Your Friends List has been updated!";
+      this.snackbar = true;
+    }
+  }
 };
 </script>
